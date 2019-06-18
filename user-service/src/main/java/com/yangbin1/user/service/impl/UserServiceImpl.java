937925d72c462d70f3bf78ec78a2dd6ad31c94da
yangbin1 <yangbin199191@163.com>
2019-06-18 18:39:02 +0800

    wwww222

--- user-service/src/main/java/com/yangbin1/user/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/yangbin1/user/service/impl/UserServiceImpl.java
@@ -1,10 +1,8 @@
 package com.yangbin1.user.service.impl;
 
 import com.yangbin1.user.service.UserService;
-import org.springframework.stereotype.Service;
 
 //@Service(version = "1.0.0")
-@Service
 public class UserServiceImpl implements UserService {
 
     public String sayHello() {
