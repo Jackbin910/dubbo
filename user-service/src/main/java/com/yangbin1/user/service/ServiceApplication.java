12812456431a36092eac57679782723d117ee802
yangbin1 <yangbin199191@163.com>
2019-06-18 18:40:33 +0800

    wwww222

--- user-service/src/main/java/com/yangbin1/user/service/ServiceApplication.java
+++ user-service/src/main/java/com/yangbin1/user/service/ServiceApplication.java
@@ -2,8 +2,10 @@ package com.yangbin1.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
 @EnableAutoConfiguration
+@ImportResource("provider.xml")
 public class ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class);
