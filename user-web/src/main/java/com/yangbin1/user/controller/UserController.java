ee5cc25c6ddb1bd0e2ccd4f6ed4d1943c48f6aa2
yangbin1 <yangbin199191@163.com>
2019-06-19 21:11:24 +0800

    33

--- user-web/src/main/java/com/yangbin1/user/controller/UserController.java
+++ user-web/src/main/java/com/yangbin1/user/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    //@Reference(version = "1.0.0",url = "dubbo://127.0.0.1:12345")
+    //@Reference(version = "1.0.0", url = "dubbo://127.0.0.1:12345",check = false)
     @Autowired
     private UserService userService;
 
