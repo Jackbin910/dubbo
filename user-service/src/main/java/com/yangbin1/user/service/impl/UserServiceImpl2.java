57e2e538075e0664776615afe4748a343134c0b1
yangbin1 <yangbin199191@163.com>
2019-06-22 21:58:39 +0800

    wwww222

---
+++ user-service/src/main/java/com/yangbin1/user/service/impl/UserServiceImpl2.java
@@ -0,0 +1,12 @@
+package com.yangbin1.user.service.impl;
+
+import com.yangbin1.user.service.UserService;
+
+//@Service(version = "1.0.0")
+public class UserServiceImpl2 implements UserService {
+
+    public String sayHello() {
+        return "hello world";
+    }
+
+}
